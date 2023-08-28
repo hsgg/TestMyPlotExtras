a29ac91aea58ab88187b8683137abaf123f217a8
Henry Gebhardt <gebhardt@caltech.edu>
2023-08-28 16:44:21 -0700

    a workaround

--- MyPlotExtras/src/MyPlotExtras.jl
+++ MyPlotExtras/src/MyPlotExtras.jl
@@ -3,6 +3,9 @@ module MyPlotExtras
 using PythonPlot
 
 function __init__()
+    # Workaround from https://github.com/cjdoris/PythonCall.jl/pull/328
+    ccall(:jl_generating_output, Cint, ()) == 1 && return nothing
+
     rcParams = PythonPlot.matplotlib.rcParams
 end
 
