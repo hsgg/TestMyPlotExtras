d60e8aa38a7a7cc5308a34382b981c061796a5bb
Henry Gebhardt <gebhardt@caltech.edu>
2023-08-29 11:10:53 -0700

    nicer workaround

--- MyPlotExtras/src/MyPlotExtras.jl
+++ MyPlotExtras/src/MyPlotExtras.jl
@@ -1,10 +1,15 @@
 module MyPlotExtras
 
 using PythonPlot
+using PythonCall
 
 function __init__()
-    # Workaround from https://github.com/cjdoris/PythonCall.jl/pull/328
-    ccall(:jl_generating_output, Cint, ()) == 1 && return nothing
+    ## Workaround from https://github.com/cjdoris/PythonCall.jl/pull/328
+    #ccall(:jl_generating_output, Cint, ()) == 1 && return nothing
+
+    # Nicer workaround:
+    # https://discourse.julialang.org/t/matplotlib-rcparams-in-module-init-function-segfault-on-m1/103314/3
+    PythonCall.pyisnull(PythonPlot.matplotlib) && return
 
     rcParams = PythonPlot.matplotlib.rcParams
 end
